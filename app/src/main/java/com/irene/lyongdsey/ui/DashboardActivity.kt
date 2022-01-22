1ceb68b873ec3814a36c2cff398992edfcd42299
irene <irene.nus8@gmail.com>
2022-01-22 10:52:29 +0100

    Add first ui of the app

--- app/src/main/java/com/irene/lyongdsey/ui/DashboardActivity.kt
+++ app/src/main/java/com/irene/lyongdsey/ui/DashboardActivity.kt
@@ -1,11 +1,12 @@
-package com.irene.lyongdsey
+package com.irene.lyongdsey.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.irene.lyongdsey.R
 
-class MainActivity : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_dashboard)
     }
 }
