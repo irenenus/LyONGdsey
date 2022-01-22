4302563a6050e9e12791cb37617025907900d8a5
irene <irene.nus8@gmail.com>
2022-01-22 14:17:48 +0100

    Add DashboardActivity.kt ui and logic, Set initial values

--- app/src/main/java/com/irene/lyongdsey/ui/DashboardActivity.kt
+++ app/src/main/java/com/irene/lyongdsey/ui/DashboardActivity.kt
@@ -7,11 +7,25 @@ import com.irene.lyongdsey.R
 import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class DashboardActivity : AppCompatActivity() {
+    private var totalFundedProjects: Int = 0
+    private var totalStakeholders: Int = 0
+    private var totalMoney: Float = 0f
+    private var averageFundsPerProject: Float = 0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
+        setUpView()
         onListeners()
+    }
+
+    private fun setUpView(){
+        numFundedProjects.text = totalFundedProjects.toString()
+        numStakeholders.text = totalStakeholders.toString()
+        raisedMoney.text = "$totalMoney €"
+        averageFunds.text = "$averageFundsPerProject €"
+
     }
 
     private fun onListeners() {
