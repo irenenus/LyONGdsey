aa89d12d77be5b88dcd65797d1871fb614feca2f
irene <irene.nus8@gmail.com>
2022-01-22 11:51:51 +0100

    Add StakeholderActivity.kt and ui and change general theme

--- app/src/main/java/com/irene/lyongdsey/ui/DashboardActivity.kt
+++ app/src/main/java/com/irene/lyongdsey/ui/DashboardActivity.kt
@@ -1,12 +1,27 @@
 package com.irene.lyongdsey.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.irene.lyongdsey.R
+import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        onListeners()
+    }
+
+    private fun onListeners() {
+
+        addStakeholderButton.setOnClickListener {
+            startActivity(Intent(this, StakeholderActivity::class.java))
+        }
+
+        addProjectButton.setOnClickListener {
+            startActivity(Intent(this, StakeholderActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/irene/lyongdsey/ui/StakeholderActivity.kt
@@ -0,0 +1,18 @@
+package com.irene.lyongdsey.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.irene.lyongdsey.R
+
+class StakeholderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stakeholder)
+
+        onListeners()
+    }
+
+    private fun onListeners() {
+
+    }
+}
